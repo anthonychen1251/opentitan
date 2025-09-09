511bb9ffb3c4d4f63f78f8e1d2e045d339533da5
Yi-Hsuan Deng <yhdeng@google.com>
2025-09-09 15:24:25 +0800

    [cov] update base after merge
    Change-Id: If57addfea1f872a04700c1f391fc532f7d7470b0
    Signed-off-by: Yi-Hsuan Deng <yhdeng@google.com>

--- sw/device/coverage/util/show_diff.py
+++ sw/device/coverage/util/show_diff.py
@@ -1,7 +1,7 @@
 import subprocess
 import re
 
-base_commit = '011b6ea1'
+base_commit = 'd742a231'
 
 with open('./bazel-out/_coverage/view/all_views.dat') as f:
   files = re.findall(r'SF:(.*)\n', f.read())
