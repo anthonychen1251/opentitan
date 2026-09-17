066a6258c456670be4f382c5afad02814825c9f3
Anthony Chen <antchen@google.com>
2026-09-18 01:05:17 +0800

    [cov] Increase crypto host harness timeouts for coverage instrumentation

--- sw/host/tests/crypto/x25519_kat/src/main.rs
+++ sw/host/tests/crypto/x25519_kat/src/main.rs
@@ -32,7 +32,7 @@ struct Opts {
     init: InitializeTest,
 
     // Console receive timeout.
-    #[arg(long, value_parser = humantime::parse_duration, default_value = "10s")]
+    #[arg(long, value_parser = humantime::parse_duration, default_value = "250s")]
     timeout: Duration,
 
     // Reduce number of tests that are run by this factor.
@@ -209,7 +209,7 @@ fn test_x25519(opts: &Opts, transport: &TransportWrapper) -> Result<()> {
         }
     }
     CryptotestCommand::Quit.send(&spi_console_device)?;
-    let _ = UartConsole::wait_for(&spi_console_device, r"PASS!|FAIL!", opts.timeout * 10)?;
+    let _ = UartConsole::wait_for(&spi_console_device, r"PASS!|FAIL!", opts.timeout * 60)?;
     assert_eq!(
         0,
         failures.len(),
